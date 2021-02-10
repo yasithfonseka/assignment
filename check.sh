aa2373ba3504644e7c4c0765abc35c9b9590ab21
Yasith S Fonseka <59591127+yasithfonseka@users.noreply.github.com>
2021-02-10 14:32:33 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if wget --spider -S "ec2-13-126-4-143.ap-south-1.compute.amazonaws.com" 2>&1 | grep -w "200\|301" ; then
+if wget --spider -S "ec2-13-126-4-143.ap-south-1.compute.amazonaws.com" 2>&1 | grep -w "200\|301" ; then    #check whether server is up or not
     echo "Server is up"
 else
     echo "Server is down. Wait till server is starting..."
-    sudo systemctl start httpd
+    sudo systemctl start httpd  #start apache server if it is down
     sudo systemctl enable httpd
 fi
