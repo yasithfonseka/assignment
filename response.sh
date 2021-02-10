57f298d0c61fdb482fa95b6f11b6783ba1703606
Yasith S Fonseka <59591127+yasithfonseka@users.noreply.github.com>
2021-02-10 14:36:05 +0530

    Update response.sh

--- response.sh
+++ response.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DATETIME=`date +%b-%d-%y`
+DATETIME=`date +%b-%d-%y` #get date 
 
-webserv="http://ec2-13-126-4-143.ap-south-1.compute.amazonaws.com/"
+webserv="http://ec2-13-126-4-143.ap-south-1.compute.amazonaws.com/" #created public server
 
-Keyword="Hello World" # enter the keyword for test content
+Keyword="Hello World" # enter the keyword for test content   #content of the server
 
-if curl -s "$webserv" | grep "$keyword"
+if curl -s "$webserv" | grep "$keyword" #check the content of the web server
 then
     # if the keyword is in the conent
     echo " the website is working fine"| curl --head "$webserv" > $DATETIME.txt
